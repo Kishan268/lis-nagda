ce1f7dd787c71f846360f4a620321e72cc28f257
Laxyo <itsolutionsofts@gmail.com>
2020-11-11 13:57:14 +0530

    compose sms model add

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,4 +1,11 @@
 <?php 
+
+const SENDTO = [
+	'A' => 'Send to All',
+	'S'	=> 'Send to Student',
+	'T' => 'Send to Teacher'
+];
+
 if(!function_exists('student_gender')){
 	function student_gender(){
 		return 'hello';
